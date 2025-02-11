ba7d0e64ca2df2512dc6de06e873620d0d5542cf
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-02-11 17:36:53 +0530

    feat: implement password hashing in user model before saving

--- models/user.js
+++ models/user.js
@@ -35,5 +35,21 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+
+  next();
+});
+
 const User = model("user", userSchema);
 module.exports = User;
