529fb6892fcdb608a1bb89d082051c190de017b9
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-02-11 18:19:54 +0530

    feat: integrate MongoDB connection and update signup view with improved styling

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8000;
+const DB_URI = process.env.DB_URI;
+
+//! Database
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 //! Middleware
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+app.use(express.urlencoded({ extended: false }));
 
 //! Routes
 app.get("/", (req, res) => {
