80b9538e57b9b6555095350901dfcdc405b0c267
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-02-11 17:36:41 +0530

    feat: add user model with schema for authentication and roles

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageURL: {
+      type: String,
+      default: "/public/images/default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("user", userSchema);
+module.exports = User;
