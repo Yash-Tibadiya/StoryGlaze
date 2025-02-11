3ec52130aaff444d8dee519da961c154364428de
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-02-11 17:52:44 +0530

    feat: add signup view and user route for handling signup requests

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 
+const userRoute = require("./routes/user");
+
 const app = express();
 const PORT = 8000;
 
@@ -12,6 +14,8 @@ app.set("views", path.resolve("./views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
