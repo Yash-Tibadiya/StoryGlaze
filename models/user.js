03c4e76c4a2143658c1f6671213b2976c1f6637c
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-02-12 17:06:13 +0530

    feat: update user profile image handling and improve comment display

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ const userSchema = new Schema(
     },
     profileImageURL: {
       type: String,
-      default: "/images/default.png",
+      default: "",
     },
     role: {
       type: String,
