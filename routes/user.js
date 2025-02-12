4094e3459f5490a2ea6fcbf7d0eb81fa20354356
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-02-12 11:10:07 +0530

    feat: update password matching to generate JWT token and add authentication service

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.get("/signup", (req, res) => {
 
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
-  await User.matchPassword(email, password);
+  await User.matchPasswordAndGenerateToken(email, password);
 
   return res.redirect("/");
 });
